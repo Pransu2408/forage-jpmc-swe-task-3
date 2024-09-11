883a73d429206b899d9bbc73e9e3a5a9a3a4fe34
Pransu2408 <pransukumar@gmail.com>
2024-09-11 14:21:51 +0530

    t3

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -22,12 +22,14 @@ class Graph extends Component<IProps, {}> {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
-    const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
-    };
+    const schema = { price_abc: 'float',
+                     price_def: 'float', 
+                     ratio: 'float', 
+                     timestamp: 'date',
+                     upper_bound: 'float',
+                     lower_bound: 'float', 
+                     trigger_alert: 'float',
+                   };
 
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
@@ -35,15 +37,17 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
+      elem.setAttribute('view', 'y line');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
+      price_abc: 'avg',
+      price_def: 'avg',
+      ratio: 'avg',
+      timestamp: 'distinct count',
+      upper_bound: 'avg',
+      lower_bound: 'avg',
+      trigger_alert: 'avg',
       }));
     }
   }
